1591fb20bdac9ccc09ea07fe68c632eb9230c65d
Pamela Frieze <pfrieze@gmail.com>
2017-11-18 20:06:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,8 @@ class Anagram
     @word = word
   end
 
-  def match(word) #take in an array and return all matches in an array 
+  def match(word) #take in an array and return all matches in an array
     word.collect {|w| } 
-
+else []
   end
 end
